5bc2e66ac10a704b4a8cec1a8acefe4f98e8f6db
Nicolas <nicolasalmeida321@gmail.com>
2025-02-21 23:36:58 -0300

    ➕ Add Message

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_22_022507) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_22_023511) do
   create_table "chats", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "chat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
+  end
+
+  add_foreign_key "messages", "chats"
 end
