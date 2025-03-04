0df37eeebadcf41b5873ee60a45ac9065273a5ae
Nicolas <nicolasalmeida321@gmail.com>
2025-03-04 11:07:51 -0300

    🖥️ add action index to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :registrations, only: %i[ new create ]
+
   resource  :session
+  get "sessions", to: "sessions#index"
+
   resources :passwords, param: :token
+
   resources :chats do
     resources :messages, except: %i[ new show ]
   end
