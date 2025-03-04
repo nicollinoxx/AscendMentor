030739a27f0ebc340c8bd4e30bbdb1d0d0cae097
Nicolas <nicolasalmeida321@gmail.com>
2025-03-04 12:21:48 -0300

    🖊️ Add name to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  validates :name, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_03_125026) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_04_143622) do
   create_table "chats", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_03_125026) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
